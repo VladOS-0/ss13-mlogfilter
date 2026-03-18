8aeb1a607ca37ef192dec59f29202517a78f8157
Vladislav Stolyarchuk <vladstolyarchuk7@gmail.com>
2026-03-18 17:11:22 +0300

    v0.3.0

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,20 @@ use std::{
     time::Instant,
 };
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 use crate::config::Config;
 
 mod config;
 
-/// Simple CLI utility to filter the Space Station 13 saved chat logs
+/// Simple CLI utility to filter Space Station 13 saved chat logs
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
     /// Paths to chat log files to filter
-    #[arg(short, long, value_name = "FILES")]
     paths: Vec<PathBuf>,
 
-    /// Paths to the output files. Defaults to "{out_dir}/filtered_{INPUT FILE NAME}". out_dir defaults to current working
+    /// Paths to output files. Defaults to "{out_dir}/filtered_{INPUT FILE NAME}". out_dir defaults to current working
     /// directory the program's working directory. Missing directories in the path will be created recursively. If more
     /// paths than outputs were provided, missing outputs will be set to default. If more outputs than paths
     /// were provided, excessive outputs will be ignored.
@@ -29,18 +28,18 @@ struct Cli {
 
     /// Path to the directory, which will be considered base for default outputs. Missing directories in the path will be
     /// created recursively.
-    #[arg(long, value_name = "DIR")]
+    #[arg(short = 'O', long, value_name = "DIR")]
     out_dir: Option<PathBuf>,
 
-    /// Read paths from standard input, separated by whitespaces
+    /// How program will use standard input. Default is "none"
     #[arg(long)]
-    stdin: bool,
+    stdin: Option<StdinMode>,
 
     /// Exits the program if failed to filter one or more paths
     #[arg(long)]
     strict: bool,
 
-    /// Allow overwrite of the output file
+    /// Allow overwrite of output files
     #[arg(long)]
     overwrite: bool,
 
@@ -63,6 +62,18 @@ struct Cli {
     /// Path to a config file
     #[arg(short, long, value_name = "FILE")]
     config: Option<PathBuf>,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Default)]
+enum StdinMode {
+    /// Standard input will be ignored
+    #[default]
+    None,
+    /// Filter will try to read one chat log from standard input to filter. If "--outputs" / "-o" is set, the first output path will be taken
+    /// for stdin log. Otherwise it will be set to "filtered_stdin.html"
+    Log,
+    /// Filter will try to read paths to chat logs separated with whitespaces from standard input
+    Path,
 }
 
 fn main() {
@@ -89,43 +100,82 @@ fn main() {
             });
     }
 
-    if cli.stdin {
-        let mut buf: Vec<u8> = Vec::new();
-        stdin().read_to_end(&mut buf).unwrap_or_else(|err| {
-            eprintln!("Failed to read from the standard input: {}", err);
-            exit(1);
-        });
-        let mut stdin_paths: Vec<PathBuf> = String::from_utf8_lossy(&buf)
-            .split_whitespace()
-            .map(|path| path.into())
-            .collect();
-        println!(
-            "Parsed {} paths from the standard input.",
-            stdin_paths.len()
-        );
-        cli.paths.append(&mut stdin_paths);
-    }
+    // Source (stdin or path to the log), path to output and log string
+    let mut logs: Vec<(String, PathBuf, String)> = Vec::with_capacity(cli.paths.len());
 
-    if cli.paths.is_empty() {
-        eprintln!("No valid paths were provided");
-        exit(1)
+    match cli.stdin.unwrap_or_default() {
+        StdinMode::None => {}
+        StdinMode::Log => {
+            let mut log: String = String::new();
+            stdin().read_to_string(&mut log).unwrap_or_else(|err| {
+                eprintln!("Failed to read from standard input: {}", err);
+                exit(1);
+            });
+            logs.push((
+                "stdin".to_string(),
+                get_path_for_output(0, &cli.outputs, None, &cli.out_dir),
+                log,
+            ));
+        }
+        StdinMode::Path => {
+            let mut stdin_paths: String = String::new();
+            stdin()
+                .read_to_string(&mut stdin_paths)
+                .unwrap_or_else(|err| {
+                    eprintln!("Failed to read from standard input: {}", err);
+                    exit(1);
+                });
+            let mut stdin_paths: Vec<PathBuf> = stdin_paths
+                .split_whitespace()
+                .map(|path| path.into())
+                .collect();
+            println!("Parsed {} paths from standard input.", stdin_paths.len());
+            cli.paths.append(&mut stdin_paths);
+        }
     }
 
     for (index, log_path) in cli.paths.iter().enumerate() {
-        let this_path_start = Instant::now();
-        let output_path = get_path_for_output(index, &cli.outputs, log_path, &cli.out_dir);
+        let index = match cli.stdin {
+            Some(StdinMode::Log) => index + 1,
+            _ => index,
+        };
 
-        match process_path(log_path, &output_path, &config, cli.overwrite) {
+        let output_path = get_path_for_output(index, &cli.outputs, Some(log_path), &cli.out_dir);
+
+        match read_to_string(log_path) {
+            Ok(log) => {
+                logs.push((log_path.to_string_lossy().into(), output_path, log));
+            }
+            Err(err) => {
+                eprintln!(
+                    "Failed to read input path {}: {}",
+                    log_path.to_string_lossy(),
+                    err
+                );
+                continue;
+            }
+        }
+    }
+
+    if logs.is_empty() {
+        eprintln!("No valid logs were provided. Use \"--help\" argument for help.");
+        exit(1)
+    }
+
+    for (source, output_path, log) in &logs {
+        let this_path_start = Instant::now();
+
+        match process_log(log, output_path, &config, cli.overwrite) {
             Ok(()) => {
                 println!(
                     "Filtered chat log from {} to {} in {}ms",
-                    log_path.to_string_lossy(),
+                    source,
                     output_path.to_string_lossy(),
                     this_path_start.elapsed().as_millis()
                 );
             }
             Err(err) => {
-                eprintln!("Failed to process {}: {}", log_path.to_string_lossy(), err);
+                eprintln!("Failed to process {}: {}", source, err);
                 if cli.strict {
                     eprintln!("Encountered error in strict mode. Exiting...");
                     exit(1)
@@ -138,7 +188,7 @@ fn main() {
 
     println!(
         "Filtered {} logs in {}ms",
-        cli.paths.len(),
+        logs.len(),
         start.elapsed().as_millis()
     );
 }
@@ -146,7 +196,7 @@ fn main() {
 fn get_path_for_output(
     index: usize,
     outputs: &[PathBuf],
-    path: &Path,
+    path: Option<&Path>,
     base_dir: &Option<PathBuf>,
 ) -> PathBuf {
     if let Some(output) = outputs.get(index) {
@@ -156,24 +206,24 @@ fn get_path_for_output(
         Some(dir) => dir.to_string_lossy().trim_end_matches("/").to_string(),
         None => ".".to_string(),
     };
-    let file_name = path
-        .file_name()
-        .map(|file_name| file_name.to_string_lossy())
-        .unwrap_or(format!("file_name_error{}", index).into());
+    let file_name = match path {
+        Some(path) => path
+            .file_name()
+            .map(|file_name| file_name.to_string_lossy())
+            .unwrap_or(format!("file_name_error{}", index).into()),
+        None => std::borrow::Cow::Borrowed("stdin"),
+    };
 
     PathBuf::from(format!("{}/filtered_{}", base_dir, file_name))
 }
 
-fn process_path(
-    path: &PathBuf,
+fn process_log(
+    log: &String,
     output_path: &PathBuf,
     config: &Config,
     overwrite: bool,
 ) -> Result<(), anyhow::Error> {
-    let chat_log = read_to_string(path)
-        .map_err(|err| anyhow::format_err!("error while reading the input file: {}", err))?;
-
-    let filtered_chat_log = filter_chat_log(chat_log, config).unwrap_or_else(|err| {
+    let filtered_chat_log = filter_chat_log(log, config).unwrap_or_else(|err| {
         eprintln!("filter error: {}", err);
         exit(1);
     });
@@ -198,8 +248,8 @@ fn process_path(
         .open(output_path)
         .unwrap_or_else(|err| {
             eprintln!(
-                "error while creating the output file in {}: {}",
-                path.to_string_lossy(),
+                "error while creating an output file {}: {}",
+                output_path.to_string_lossy(),
                 err
             );
             exit(1);
@@ -209,7 +259,7 @@ fn process_path(
         .write_all(filtered_chat_log.as_bytes())
         .unwrap_or_else(|err| {
             eprintln!(
-                "error while writing to the output file in {}: {}",
+                "error while writing to an output file in {}: {}",
                 output_path.to_string_lossy(),
                 err
             );
@@ -219,7 +269,7 @@ fn process_path(
     Ok(())
 }
 
-fn filter_chat_log(chat_log: String, config: &Config) -> Result<String, anyhow::Error> {
+fn filter_chat_log(chat_log: &String, config: &Config) -> Result<String, anyhow::Error> {
     let mut output = String::with_capacity(chat_log.len());
     let parts: Vec<&str> = chat_log.split_inclusive("<div class=\"Chat\">").collect();
     if parts.len() != 2 {
